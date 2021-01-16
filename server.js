c4dc315d4568efbed00f9567d10a96f26c05a671
chomieu <chomieu@gmail.com>
2021-01-15 22:03:45 -0800

    Added routes folder

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 const express = require("express")
+const path = require("path")
+const PORT = process.env.PORT || 8080
+const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname + "index.html"))
+})
+
+app.listen(PORT, function() {
+    console.log(`App listening on PORT: ${PORT}`)
+})
