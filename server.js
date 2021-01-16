4cbc1c86ce24648cd9dee3d26aba877c92eb6227
chomieu <chomieu@gmail.com>
2021-01-16 00:41:11 -0800

    get /api/notes functioning

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const path = require("path")
+const fs = require("fs")
 
 const app = express()
 const PORT = process.env.PORT || 8080
@@ -8,11 +9,17 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static("public"))
 
-app.get("/notes", function(req, res) {
-    console.log(__dirname)
+app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname + "/public/notes.html"))
 })
 
-app.listen(PORT, function() {
+app.get("/api/notes", function (req, res) {
+    fs.readFile(__dirname + "db.json", function (err, data) {
+        err ? console.error(err) : console.log("success")
+        res.json(JSON.parse(data))
+    })
+})
+
+app.listen(PORT, function () {
     console.log(`App listening on PORT: ${PORT}`)
 })
